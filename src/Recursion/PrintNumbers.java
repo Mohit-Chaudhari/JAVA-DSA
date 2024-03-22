3a24bbcdd3c890aadd6c25e56b2fbba76c792f9e
Mohit <chaudharimohit39@gmail.com>
2024-03-23 00:26:39 +0530

    Prints numbers in descending order using recursion

--- src/Recursion/PrintNumbers.java
+++ src/Recursion/PrintNumbers.java
@@ -1,2 +1,22 @@
-package Recursion;public class PrinntNumbers {
+package Recursion;
+
+/**
+ * Prints numbers in descending order using recursion
+ * @author Mohit Chaudhari
+ */
+public class PrintNumbers {
+    public static void main(String[] args) {
+        print(10);
+    }
+
+    public static void print(int number) {
+
+        // base condition
+        if (number == 0)
+            return;
+        System.out.print(" " + number);
+
+        // recursion call
+        print(number - 1);
+    }
 }
