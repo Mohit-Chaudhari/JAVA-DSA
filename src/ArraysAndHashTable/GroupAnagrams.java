d59d1eaf164fe65aec58be0580b3c639fb8c63e4
Mohit <chaudharimohit39@gmail.com>
2024-03-25 20:43:51 +0530

    Group Anagrams.

---
+++ src/ArraysAndHashTable/GroupAnagrams.java
@@ -0,0 +1,65 @@
+package ArraysAndHashTable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @author Mohit Chaudhari
+ * <a href="https://www.linkedin.com/in/mohit-chaudhari-1018/">Linkedin - Mohit Chaudhari</a>
+ * Created date: 25/03/24
+ */
+public class GroupAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String str: strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
+            }
+
+            map.get(sortedWord).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
+
+/**
+ * Problem: https://leetcode.com/problems/group-anagrams/
+ * Topics covered: Array, Hash Table, String, Sorting
+ *
+ * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+ *
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+ * typically using all the original letters exactly once.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: strs = ["eat","tea","tan","ate","nat","bat"]
+ * Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ * Example 2:
+ *
+ * Input: strs = [""]
+ * Output: [[""]]
+ * Example 3:
+ *
+ * Input: strs = ["a"]
+ * Output: [["a"]]
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= strs.length <= 104
+ * 0 <= strs[i].length <= 100
+ * strs[i] consists of lowercase English letters.
+ */
